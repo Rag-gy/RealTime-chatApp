9f97d34419a748486e10c058d6ff92fdc713d5d3
Raghul Prasanth <106253494+Rag-gy@users.noreply.github.com>
2023-03-14 19:11:40 +0530

    Update generateToken.js

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id)=>{
-    return jwt.sign({id}, process.env.SECRET_KEY, {expiresIn:'30d'})    // this will create a secret key which expires in 30 days
+    return jwt.sign({id}, THisisMadnESsasItRYtoCReAteTHis, {expiresIn:'30d'})    // this will create a secret key which expires in 30 days
 }
 
 module.exports = generateToken;
